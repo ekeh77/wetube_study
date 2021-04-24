cd9fb4e38a3cdc5163b6695f38f65d3dc5a6adbf
ekeh77 <74358756+ekeh77@users.noreply.github.com>
2021-04-24 16:48:37 +0900

    0424wetube_study

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hi!")
+import express from "express";
+import morgan from  "morgan";
+import helmet from "helmet";
+import cokieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+//const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => 
+console.log(`Listening on: http://localhost:${PORT}`);
+
+const handlehome = (req, res) => res.send("Hello from Home");
+
+const handleProfile = (req, res) => res.send("You are on my profile")
+
+app.use(cookieParser());
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+
+app.get("/", handlehome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening)
+
+/*
+// respond with "hello world" when a GET request in made to the homepage
+app.get('/', function(req, res){
+    res.send('hello world');
+})
+*/
