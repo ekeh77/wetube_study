ecce71b7e713e34a0395d450059428f47468b71a
ekeh77 <74358756+ekeh77@users.noreply.github.com>
2021-05-01 08:47:24 +0900

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,9 +10,9 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 /*
 userRouter.get(routes.users, (req, res) => res.send("Users"));
